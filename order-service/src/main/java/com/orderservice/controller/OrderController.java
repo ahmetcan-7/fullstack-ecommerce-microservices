eeab717656848044e4f8c3d62e864aac6b46219b
ahmetCan7 <ahmetcan423507@gmail.com>
2022-12-04 16:59:04 +0300

    Add zipkin for log tracing

--- order-service/src/main/java/com/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/orderservice/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.orderservice.dto.order.OrderDto;
 import com.orderservice.dto.order.CreateOrderRequest;
 import com.orderservice.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -14,13 +15,14 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/v1/orders")
 @RequiredArgsConstructor
+@Slf4j
 public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
     // @PreAuthorize("hasAnyAuthority('user:delete')")
     public ResponseEntity<OrderDto> createOrder(@Valid @RequestBody CreateOrderRequest createOrderRequest){
-
+        log.info("create order request was called");
         return new ResponseEntity<>(orderService.createOrder(createOrderRequest),HttpStatus.CREATED);
     }
 
